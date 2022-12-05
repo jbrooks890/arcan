bb3b4d3cbe7159fffbd76e190b3717c8ae47c5ad
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-12-04 22:50:01 -0500

    minors

---
+++ src/components/frags/Dashboard.js
@@ -0,0 +1,18 @@
+export default function Dashboard() {
+  return (
+    <div id="user-dashboard" className="grid">
+      <div id="welcome">Welcome, User.</div>
+      <div>Chapters read</div>
+      {/* Admin */}
+      <div id="admin-pane">
+        <ul>
+          <li>Characters</li>
+          <li>Worlds</li>
+          <li>Other</li>
+          <li>Stuff</li>
+          <li>Characters</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
